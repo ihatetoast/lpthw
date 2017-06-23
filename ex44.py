ac3f49e64db78e18c4b4b536e4b1a322ca9b5f7e
Katy <ihatetoast@gmail.com>
2017-06-22 21:17:13 -0500

    override and altered with super

--- ex44.py
+++ ex44.py
@@ -1,12 +1,32 @@
 class Parent(object):
 	def implicit(self):
-		print "PARENT implicit"
+		print "PARENT implicit()"
+	def override(self):
+		print "PARENT override()"
+	def altered(self):
+		print "PARENT altered()"
 
 class Child(Parent):
-	pass
+	def override(self):
+		print "CHILD override()"
+	def altered(self):
+		print "CHILD BEFORE parent altered"
+		super(Child, self).altered()
+		print "CHILD AFTER parent altered"
+
 
 dad = Parent()
 son = Child()
 
+print "IMPLICIT:"
 dad.implicit()
 son.implicit()
+
+print "OVERRIDE:"
+
+dad.override()
+son.override()
+
+print "ALTERED:"
+dad.altered()
+son.altered()
